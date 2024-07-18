5e4a674c358b7989e7f16a88563a359ef0d9092e
gabri3lmarques <gabri3lmarques@gmail.com>
2024-07-18 11:14:57 -0300

    finalizando o bloco

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => 'c104565beb5e01cfd56f');
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { useState, useEffect } from '@wordpress/element';
+import axios from 'axios';
+import '../editor.css';
+
+registerBlockType('nasa/planets-block', {
+  title: 'NASA Planets',
+  icon: 'universal-access-alt',
+  category: 'widgets',
+  edit: () => {
+    const [planet, setPlanet] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      const fetchPlanet = async () => {
+        try {
+          const response = await axios.get(
+            `https://api.nasa.gov/planetary/apod?api_key=GRUnQnlrJ62rUfshlMntT1g8IVY612sA5KgIkcCv`
+          );
+          setPlanet(response.data);
+          setLoading(false);
+        } catch (error) {
+          console.error('Error fetching data from NASA API', error);
+          setLoading(false);
+        }
+      };
+
+      fetchPlanet();
+    }, []);
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div className="nasa-planets-block">
+        {planet && (
+          <div>
+            <h2>{planet.title}</h2>
+            <p>{planet.explanation}</p>
+            <img src={planet.url} alt={planet.title} />
+          </div>
+        )}
+      </div>
+    );
+  },
+  save: () => {
+    return null; // Bloco dinâmico, não há necessidade de salvar conteúdo
+  }
+});
